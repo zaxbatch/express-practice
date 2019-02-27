a4e852aec2379ebc8cf3e5dd5dcb8b3f9075c79d
Dotcom <sneakjunkmail@gmail.com>
2019-02-26 23:47:02 -0500

    added content

--- app.js
+++ app.js
@@ -49,4 +49,7 @@ app.use(function(err, req, res, next) {
 
 
 
+
+
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
+var cookieParser = require('cookie-parser');
 
+router.use(cookieParser());
+
+/*
+router.get('/', function (req, res) {
+  // Cookies that have not been signed
+  console.log('Cookies: ', req.cookies)
+ 
+  // Cookies that have been signed
+  console.log('Signed Cookies: ', req.signedCookies)
+});
+
+*/
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // Cookies that have not been signed
+
   res.render('index', { title: 'Express' });
 });
 
@@ -42,4 +57,18 @@ router.get('/playground', function(req, res, next) {
   res.render('playground', { title: 'Playground' });
 });
 
+
+router.get('/third-party/cookie-parser', function(req, res, next) {
+	// Cookies that have not been signed
+  console.log('Cookies: ', req.cookies)
+ 
+  // Cookies that have been signed
+  console.log('Signed Cookies: ', req.signedCookies)
+  res.render('cookie-parser', { title: 'cookie-parser' });
+});
+
+router.get('/third-party/body-parser', function(req, res, next) {
+
+  res.render('body-parser', { title: 'body-parser' });
+});
 module.exports = router;
